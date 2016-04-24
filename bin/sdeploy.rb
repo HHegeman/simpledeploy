5836c9f8610a58a93c06481e0358ca9c1a6e3ee9
hhegeman <hhegeman@gmail.com>
2016-04-24 22:56:05 +0200

    add some to do options.

--- bin/sdeploy.rb
+++ bin/sdeploy.rb
@@ -9,11 +9,23 @@ OptionParser.new do |opts|
     puts opts
     exit
   end
+  opts.on("-i", "--install", "Clone repo to disk and add apache config.") do |ext|
+    @options[:install] = ext
+  end
+  opts.on("-u", "--update", "git pull all repos and reload apache.") do |ext|
+    @options[:update] = ext
+  end
   opts.on("-s ", "--sites", "Specify which sites should be done. (for more sites use , seperated list.) ") do |ext|
     @options[:sites] = ext
   end
   opts.on("-l", "--list", "gives a list of all configured sites.") do |ext|
     @options[:list] = ext
+  end
+  opts.on("-a", "--apache", "Regenerate apache config for given sites.") do |ext|
+    @options[:apache] = ext
+  end
+  opts.on("--delete", "delete a website (removes site from apache config and removes repo from disk)") do |ext|
+    @options[:delete] = ext
   end
   opts.on("-v", "--verbose", "output logging.") do |ext|
     @options[:verbose] = ext
@@ -30,4 +42,5 @@ end
 
 # check if given options with -s are actually posible else raise error.
 Sort.input(Conf.sitelist, @options[:sites])
+
 puts Log.getlog
